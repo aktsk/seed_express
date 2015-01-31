26e84713f56db917f34af6f9daffb16bf5f68c35
takakiNG <ttakaki@aktsk.jp>
2015-01-31 21:23:09 +0900

    Implements SeedTable.get_record & SeedTable#disable_record_cache

--- app/models/seed_table.rb
+++ app/models/seed_table.rb
@@ -1,3 +1,24 @@
 class SeedTable < ActiveRecord::Base
   has_many :seed_records
+
+  def self.get_record(object)
+    case object
+    when String
+      table_name = object
+    when Symbol
+      table_name = object.to_sym
+    when ActiveRecord::Base
+      table_name = object.class.table_name
+    when Class
+      table_name = object.table_name
+    end
+
+    self.where(:table_name => table_name).first || self.create!(table_name: table_name)
+  end
+
+  def disable_record_cache(ids = nil)
+    query = self.seed_records
+    query = query.where(:record_id => ids) if ids
+    query.update_all(:digest => nil)
+  end
 end
