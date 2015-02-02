bb81265d902bde25e80df4980f44ba9913efe195
takakiNG <ttakaki@aktsk.jp>
2015-02-02 12:44:43 +0900

    Removes a migration file becuase the file occurs migration check error

--- db/migrate/20150131182403_be_nullable_digest_on_seed_records.rb
+++
@@ -1,9 +0,0 @@
-class BeNullableDigestOnSeedRecords < ActiveRecord::Migration
-  def self.up
-    change_column :seed_records, :digest, :string, :null => true
-  end
-
-  def self.down
-    change_column :seed_records, :digest, :string, :null => false
-  end
-end
