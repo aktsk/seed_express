ebdd23338a5ee8fbc883f1ac8dc0658d2e90245c
takakiNG <ttakaki@aktsk.jp>
2015-02-05 14:10:16 +0900

    Fixes a bug of migration & refactors migration files for Rails 3.2 or later

--- db/migrate/20140623225222_create_seed_records.rb
+++ db/migrate/20140623225222_create_seed_records.rb
@@ -1,6 +1,6 @@
 class CreateSeedRecords < ActiveRecord::Migration
-  def self.up
-    add_create_table :seed_records do |t|
+  def change
+    create_table :seed_records do |t|
       t.references   :seed_table, null: false
       t.integer      :record_id,  null: false
       t.string       :digest,     null: true
@@ -9,9 +9,5 @@ class CreateSeedRecords < ActiveRecord::Migration
 
     add_index :seed_records, [:seed_table_id], name: :idx_seed_records_001
     add_index :seed_records, [:seed_table_id, :record_id], unique: true, name: :idx_seed_records_002
-  end
-
-  def self.down
-    remove_table :seed_tables
   end
 end
