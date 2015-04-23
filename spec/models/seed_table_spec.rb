182b5356c8289b81bff12527c2aaac1d33c1eb0d
takakiNG <ttakaki@aktsk.jp>
2015-04-23 21:14:29 +0900

    Implements specs of SeedTable model

--- spec/models/seed_table_spec.rb
+++ spec/models/seed_table_spec.rb
@@ -51,4 +51,59 @@ describe SeedTable do
       end
     end
   end
+
+  describe "#disable_record_cache" do
+    let(:seed_table) do
+      seed_table = SeedTable.create!(:table_name => 'items',
+                                     :digest => 'ABC')
+      seed_table.disable_record_cache
+      seed_table
+    end
+
+    describe "about setting SeedTable#digest" do
+
+      it "becomes nil" do
+        expect(seed_table.digest).to be_nil
+      end
+
+      it "saves the record" do
+        expect(seed_table.reload.digest).to be_nil
+      end
+    end
+
+    describe "about disabling SeedRecord#digest" do
+      let(:seed_record_count) { 10 }
+      before do
+        seed_table.seed_records.delete_all
+        @seed_records = (1 .. seed_record_count).map do |record_id|
+          SeedRecord.create!(:seed_table_id => seed_table.id,
+                             :record_id => record_id,
+                             :digest => "D#{'%02d' % record_id}")
+        end
+      end
+
+      context "when ids of an argument is present" do
+        it "sets nil to SeedRecord#digest of specified seed_records" do
+          split_index = 5
+          record_ids = @seed_records[0, split_index].map(&:record_id)
+          seed_table.disable_record_cache(record_ids)
+          modified_digests = @seed_records[0, split_index].map(&:reload).map(&:digest)
+          expect(modified_digests).to eq([nil] * split_index)
+
+          original_digests = @seed_records[split_index, 999_999].map(&:digest)
+          modified_digests =
+            @seed_records[split_index, 999_999].map(&:reload).map(&:digest)
+          expect(modified_digests).to eq original_digests
+        end
+      end
+
+      context "when ids of an argument is blank" do
+        it "sets nil to SeedRecord#digest of related seed_records" do
+          seed_table.disable_record_cache
+          modified_digests = @seed_records.map(&:reload).map(&:digest)
+          expect(modified_digests).to eq ([nil] * seed_record_count)
+        end
+      end
+    end
+  end
 end
