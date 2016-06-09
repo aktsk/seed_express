a6e0592739da45e033d9872d09e2ef562ad29713
hyounsub.shin <hyounsub.shin@hyounsubshin-no-MacBook-Pro.local>
2016-06-09 16:54:17 +0900

    seedの拡張子を*.hashから*.rbに変更

--- lib/seed_express/rubyhash.rb
+++ lib/seed_express/rubyhash.rb
@@ -2,19 +2,19 @@
 module SeedExpress
   class RubyHash < Abstract
     def file_name
-      @file_name ||= "#{@path}/#{table_name}.hash"
+      @file_name ||= "#{@path}/#{table_name}.rb"
     end
 
     def in_records
       return @in_records if @in_records
       callbacks[:before_reading_data].call
 
-      # 静的チェックツールからセキュリティリスクだと言われるが、決められたファイルのみ評価する前提のため無視
+      # Rubocop say "The use of `eval` is a serious security risk",
+      # but that is not a risk because `eval` execute only a specified file
       @in_records = eval File.read file_name
 
       callbacks[:after_reading_data].call(@in_records.size)
       @in_records
     end
-
   end
 end
