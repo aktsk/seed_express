ee7b8c446869daceb3c6bc347bcd9aef469da529
Toshiyuki Takaki <ttakaki@aktsk.jp>
2016-06-23 23:12:21 +0900

    Implements SeedExpress::Supporters to use Enumerable#to_h and ActiveRecord::Relation#pluck

---
+++ lib/seed_express/supporters.rb
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+module SeedExpress
+  class Supporters
+    class << self
+      def regist!
+        define_to_h
+        define_pluck
+      end
+
+      private
+
+      def define_to_h
+        Enumerable.class_eval do
+          unless self.instance_methods.include?(:to_h)
+            define_method :to_h do
+              self.inject({}) { |h, (k, v)| h[k] = v; h }
+            end
+          end
+        end
+      end
+
+      def define_pluck
+        ActiveRecord::Relation.class_eval do
+          unless self.instance_methods.include?(:pluck)
+            define_method :pluck do |args|
+              if Array === args
+                self.select(args).map { |v| args.map { |column| v.send(column) }}
+              else
+                self.select(args).map { |v| v.send(args) }
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
