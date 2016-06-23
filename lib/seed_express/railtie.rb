f9321c998f299092ace732425e8d856397b49e4e
Toshiyuki Takaki <ttakaki@aktsk.jp>
2016-06-23 23:06:51 +0900

    Requies model files automatically

--- lib/seed_express/railtie.rb
+++ lib/seed_express/railtie.rb
@@ -1,7 +1,8 @@
 class Railtie < Rails::Railtie
   initializer "seed_express" do
     Rails.application.config.paths["db/migrate"] << "#{File.dirname(__FILE__)}/../../db/migrate"
-    load "#{File.dirname(__FILE__)}/../../app/models/seed_record.rb"
-    load "#{File.dirname(__FILE__)}/../../app/models/seed_table.rb"
+    Dir.glob("#{File.dirname(__FILE__)}/../../app/models/*.rb").each do |file|
+      load file
+    end
   end
 end
