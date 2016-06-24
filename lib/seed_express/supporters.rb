d56cf457e5dc028c4e8b4434aee99830116dcfca
Toshiyuki Takaki <ttakaki@aktsk.jp>
2016-06-24 09:29:44 +0900

    Becommes shallow blocks

--- lib/seed_express/supporters.rb
+++ lib/seed_express/supporters.rb
@@ -11,23 +11,21 @@ module SeedExpress
 
       def define_to_h
         Enumerable.class_eval do
-          unless self.instance_methods.include?(:to_h)
-            define_method :to_h do
-              self.inject({}) { |h, (k, v)| h[k] = v; h }
-            end
+          return if self.instance_methods.include?(:to_h)
+          define_method :to_h do
+            self.inject({}) { |h, (k, v)| h[k] = v; h }
           end
         end
       end
 
       def define_pluck
         ActiveRecord::Relation.class_eval do
-          unless self.instance_methods.include?(:pluck)
-            define_method :pluck do |args|
-              if Array === args
-                self.select(args).map { |v| args.map { |column| v.send(column) }}
-              else
-                self.select(args).map { |v| v.send(args) }
-              end
+          return if self.instance_methods.include?(:pluck)
+          define_method :pluck do |args|
+            if Array === args
+              self.select(args).map { |v| args.map { |column| v.send(column) }}
+            else
+              self.select(args).map { |v| v.send(args) }
             end
           end
         end
