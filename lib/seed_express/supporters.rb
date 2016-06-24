f59a1a57d4c03db5fd3dbba5d5da88976658175d
Toshiyuki Takaki <ttakaki@aktsk.jp>
2016-06-24 09:35:43 +0900

    Fixes complexities (I hope)

--- lib/seed_express/supporters.rb
+++ lib/seed_express/supporters.rb
@@ -12,7 +12,7 @@ module SeedExpress
       def define_to_h
         Enumerable.class_eval do
           return if self.instance_methods.include?(:to_h)
-          define_method :to_h do
+          def to_h
             self.inject({}) { |h, (k, v)| h[k] = v; h }
           end
         end
@@ -21,7 +21,7 @@ module SeedExpress
       def define_pluck
         ActiveRecord::Relation.class_eval do
           return if self.instance_methods.include?(:pluck)
-          define_method :pluck do |args|
+          def pluck
             if Array === args
               self.select(args).map { |v| args.map { |column| v.send(column) }}
             else
