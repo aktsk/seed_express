0549c7e35b65e2d9733e575eb63c6f000f44187c
hyounsub.shin <hyounsub.shin@hyounsubshin-no-MacBook-Pro.local>
2016-06-30 15:32:33 +0900

    誤ったコンプリート解消の修正

--- lib/seed_express.rb
+++ lib/seed_express.rb
@@ -8,8 +8,6 @@ require "seed_express/version"
 module SeedExpress
   autoload :Railtie,    'seed_express/railtie'
   Railtie
-  autoload :Abstract, 'seed_express/abstract'
-  autoload :CSV,      'seed_express/csv'
   autoload :Abstract,   'seed_express/abstract'
   autoload :CSV,        'seed_express/csv'
   autoload :Parts,      'seed_express/parts'
