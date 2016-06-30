e0dd686c36b78a4bf9219e0287b7ecb9150d03c3
hyounsub.shin <hyounsub.shin@hyounsubshin-no-MacBook-Pro.local>
2016-06-30 19:00:41 +0900

    バージョンアップに伴い、ruby_hash.rbのどころも対応

--- lib/seed_express/ruby_hash.rb
+++ lib/seed_express/ruby_hash.rb
@@ -1,20 +1,11 @@
 # -*- coding: utf-8 -*-
 module SeedExpress
   class RubyHash < Abstract
-    def file_name
-      @file_name ||= "#{@path}/#{table_name}.rb"
-    end
+    require 'tempfile'
+    FILE_SUFFIX = 'rbhash'
 
-    def in_records
-      return @in_records if @in_records
-      callbacks[:before_reading_data].call
-
-      # Rubocop says, "The use of `eval` is a serious security risk",
-      # but that is not a risk because `eval` execute only a specified file
-      @in_records = eval File.read file_name
-
-      callbacks[:after_reading_data].call(@in_records.size)
-      @in_records
+    def read_values_from(data)
+      eval(data)[:records]
     end
   end
 end
