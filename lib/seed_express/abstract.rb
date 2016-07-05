8bd6dd1eee1ebe1337e575aa09f656bf072809dd
Toshiyuki Takaki <ttakaki@aktsk.jp>
2016-07-05 22:06:22 +0900

    Fixes an error of 'after_seed_express_validation' because #target_model is expected to return a model class but the method returned ActiveRecord::Relation

--- lib/seed_express/abstract.rb
+++ lib/seed_express/abstract.rb
@@ -51,7 +51,7 @@ module SeedExpress
       unless v = self.class.table_to_klasses[@table_name]
         raise "#{@table_name} isn't able to convert to a class object"
       end
-      v.unscoped
+      v
     end
     memoize :target_model
 
