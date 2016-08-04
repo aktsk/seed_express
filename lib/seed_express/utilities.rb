d355b30624d6befab0bf734baf847bae273cf8b2
Toshiyuki Takaki <ttakaki@aktsk.jp>
2016-08-04 18:01:35 +0900

    Fixes a complex method

--- lib/seed_express/utilities.rb
+++ lib/seed_express/utilities.rb
@@ -15,28 +15,21 @@ module SeedExpress
     end
 
     def do_each_block(array, block_size, whole_callback_name, block_callback_name)
-      processed_size = 0
-      args_lambda =
-        if self.respond_to?(:part_total)
-          -> { [self.part_count, self.part_total, processed_size, array.size] }
-        else
-          -> { [processed_size, array.size] }
-        end
-
       whole_callback_before,
       whole_callback_after,
       block_callback_before,
       block_callback_after =
         callbacks_for_do_each_block(whole_callback_name, block_callback_name)
 
-      whole_callback_before.call(*args_lambda.call)
+      processed_size = 0
+      whole_callback_before.call(*args_for_callbacks(processed_size, array))
       array.each_slice(block_size) do |targets|
-        block_callback_before.call(*args_lambda.call)
+        block_callback_before.call(*args_for_callbacks(processed_size, array))
         yield(targets)
         processed_size += targets.size
-        block_callback_after.call(*args_lambda.call)
+        block_callback_after.call(*args_for_callbacks(processed_size, array))
       end
-      whole_callback_before.call(*args_lambda.call)
+      whole_callback_before.call(*args_for_callbacks(processed_size, array))
     end
 
     def callbacks_for_do_each_block(whole_callback_name, block_callback_name)
@@ -51,6 +44,14 @@ module SeedExpress
        callbacks[block_callback_before],
        callbacks[block_callback_after],
       ]
+    end
+
+    def args_for_callbacks(processed_size, array)
+      if self.respond_to?(:part_total)
+        [self.part_count, self.part_total, processed_size, array.size]
+      else
+        [processed_size, array.size]
+      end
     end
   end
 end
