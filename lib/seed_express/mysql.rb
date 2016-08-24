1fad1af69368e28382c272a731de7e55b4c61f91
Toshiyuki Takaki <ttakaki@aktsk.jp>
2016-08-24 17:57:59 +0900

    Implements SeedExpress::Mysql.set_null_validation

--- lib/seed_express/mysql.rb
+++ lib/seed_express/mysql.rb
@@ -16,6 +16,33 @@ module SeedExpress
       def sql_mode=(v)
         ActiveRecord::Base.connection.execute("SET session sql_mode = '#{v}'")
       end
+
+      def set_null_validation(model)
+        model.class_eval do
+          validate :validation_which_not_null_without_default
+
+          private
+
+          def validation_which_not_null_without_default
+            target_columns = self.class.not_null_without_default_columns
+            target_columns.each do |column|
+              next unless self[column].nil?
+              self.errors[column] << "must be set"
+            end
+          end
+
+          class << self
+            extend Memoist
+
+            def not_null_without_default_columns
+              self.columns.select do |v|
+                v.primary == false && v.null == false && v.default.nil?
+              end.map(&:name).map(&:to_sym)
+            end
+            memoize :not_null_without_default_columns
+          end
+        end
+      end
     end
   end
 end
