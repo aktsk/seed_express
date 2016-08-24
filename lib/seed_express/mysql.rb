1297977d75aa89f6acbcfcbe0fb0b0a4fd71ae7e
Toshiyuki Takaki <ttakaki@aktsk.jp>
2016-08-24 18:58:31 +0900

    Fixes a complex method

--- lib/seed_express/mysql.rb
+++ lib/seed_express/mysql.rb
@@ -19,6 +19,7 @@ module SeedExpress
 
       def set_null_validation(model)
         define_validation(model)
+        define_not_null_without_default_column_name_symbols(model)
         define_not_null_without_default_column_names(model)
         define_not_null_without_default_columns(model)
         define_is_not_null_without_default_column(model)
@@ -33,7 +34,7 @@ module SeedExpress
           private
 
           def validation_which_not_null_without_default
-            target_columns = self.class.not_null_without_default_column_names
+            target_columns = self.class.not_null_without_default_column_name_symbols
             target_columns.each do |column|
               next unless self[column].nil?
               self.errors[column] << "must be set"
@@ -42,13 +43,25 @@ module SeedExpress
         end
       end
 
-      def define_not_null_without_default_column_names(model)
+      def define_not_null_without_default_column_name_symbols(model)
         model.class_eval do
           class << self
             extend Memoist
 
+            def not_null_without_default_column_name_symbols
+              not_null_without_default_column_names.map(&:to_sym)
+            end
+            memoize :not_null_without_default_column_name_symbols
+          end
+        end
+      end
+
+      def define_not_null_without_default_column_names(model)
+        model.class_eval do
+          class << self
+            private
             def not_null_without_default_column_names
-              self.not_null_without_default_columns.map(&:name).map(&:to_sym)
+              not_null_without_default_columns.map(&:name)
             end
             memoize :not_null_without_default_column_names
           end
@@ -58,6 +71,7 @@ module SeedExpress
       def define_not_null_without_default_columns(model)
         model.class_eval do
           class << self
+            private
             def not_null_without_default_columns
               self.columns.select do |v|
                 not_null_without_default_column?(v)
@@ -70,6 +84,7 @@ module SeedExpress
       def define_is_not_null_without_default_column(model)
         model.class_eval do
           class << self
+            private
             def not_null_without_default_column?(column)
               !column.primary && !column.null && column.default.nil?
             end
