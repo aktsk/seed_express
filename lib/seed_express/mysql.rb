eea943cf90dbf6e4cfaec24596f4042b35f5416e
Toshiyuki Takaki <ttakaki@aktsk.jp>
2016-08-24 20:18:54 +0900

    The validator independs; It is contained SeedExpress::ModelValidator

--- lib/seed_express/mysql.rb
+++ lib/seed_express/mysql.rb
@@ -16,47 +16,6 @@ module SeedExpress
       def sql_mode=(v)
         ActiveRecord::Base.connection.execute("SET session sql_mode = '#{v}'")
       end
-
-      def set_null_validation(model)
-        model.class_eval do
-          validate :validation_which_not_null_without_default
-
-          private
-
-          def validation_which_not_null_without_default
-            target_columns = self.class.not_null_without_default_column_name_symbols
-            target_columns.each do |column|
-              next unless self[column].nil?
-              self.errors[column] << "must be set"
-            end
-          end
-
-          class << self
-            extend Memoist
-
-            def not_null_without_default_column_name_symbols
-              not_null_without_default_column_names.map(&:to_sym)
-            end
-            memoize :not_null_without_default_column_name_symbols
-
-            private
-
-            def not_null_without_default_column_names
-              not_null_without_default_columns.map(&:name)
-            end
-
-            def not_null_without_default_columns
-              self.columns.select do |v|
-                not_null_without_default_column?(v)
-              end
-            end
-
-            def not_null_without_default_column?(column)
-              !column.primary && !column.null && column.default.nil?
-            end
-          end
-        end
-      end
     end
   end
 end
