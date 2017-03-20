4379a2af42ac2beb90ede810e211dcc939905c35
Toshiyuki Takaki <ttakaki@aktsk.jp>
2017-03-20 23:03:00 +0900

    ActiveRecord::Relation#pluck, which is implemented in this gem, gets compatible with ActiveRecord 4.0 or later

--- lib/seed_express/supporters.rb
+++ lib/seed_express/supporters.rb
@@ -22,13 +22,24 @@ module SeedExpress
       def define_pluck
         ActiveRecord::Relation.class_eval do
           return if self.instance_methods.include?(:pluck)
-          def pluck(args)
-            Enumerable === args ? pluck_for_columns(args) : pluck_for_a_column(args)
+          def pluck(*args)
+            if args.size > 1
+              # Compatible for ActiveRecord 4.0(or later), multipluck 0.0.5(or later)
+              pluck_for_columns(*args)
+            else
+              # Compatible for multipluck 0.0.4(or earlier)
+              first = args.first
+              if Enumerable === first
+                pluck_for_columns(*first)
+              else
+                pluck_for_a_column(first)
+              end
+            end
           end
 
           private
 
-          def pluck_for_columns(args)
+          def pluck_for_columns(*args)
             self.select(args).map { |record| args.map { |column| record.send(column) } }
           end
 
